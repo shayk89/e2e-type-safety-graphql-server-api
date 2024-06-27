4267aa49e9b653a5e18109b13b58018c721ea743
Shay K <shaykemp89@gmail.com>
2024-06-27 11:39:17 -0700

    commiting basic functioning server

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// src/index.ts
+
+// 1
+import { createYoga } from "graphql-yoga";
+import { createServer } from "node:http";
+import { schema } from "./schema";
+// 2
+const port = Number(process.env.API_PORT) || 4000
+// 3
+const yoga = createYoga({
+    schema:schema
+})
+const server = createServer(yoga);
+// 4
+server.listen(port,() => {
+  console.log(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
+});
